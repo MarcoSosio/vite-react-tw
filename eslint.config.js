10982315352797fff593fd13e44a4e77c3a74bd4
Marco Sosio <marcososio007@gmail.com>
2025-08-20 23:23:37 +0200

    sistemazioni

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -12,7 +13,6 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
-      "prettier",
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -28,4 +28,5 @@ export default defineConfig([
       semi: ["error", "always"],
     },
   },
+  eslintConfigPrettier,
 ]);
